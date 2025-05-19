0f40f8e7bddad65d4885cf55716df699d28c552e
OrdogDavidZsolt <ordogdavid2002@gmail.com>
2025-05-19 13:22:39 +0200

    small edits

--- DemoApp2025Spring.Api/KonyvtarDBContext.cs
+++ DemoApp2025Spring.Api/KonyvtarDBContext.cs
@@ -10,11 +10,6 @@ namespace DemoApp2025Spring.Api
         {
         }
 
-        /*
-        public virtual DbSet<Person> People { get; set; }
-        public virtual DbSet<Item> Items { get; set; }
-        */
-
         public virtual DbSet<Konyvek> Konyvek { get; set; }
         public virtual DbSet<Kolcsonzes> Kolcsonzesek { get; set; }
         public virtual DbSet<Olvasok> Olvasok { get; set; }
